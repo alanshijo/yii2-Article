6f1573832fea0eeeef8a1514b51382278e5766f9
alanshijo <alanshijoatkl@gmail.com>
2023-11-24 12:41:03 +0530

    Project folders restructured

--- article/views/layouts/main.php
+++ article/views/layouts/main.php
@@ -39,7 +39,6 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav'],
         'items' => [
-            ['label' => 'Home', 'url' => [Yii::$app->homeUrl]],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest
                 ? (['label' => 'Login', 'url' => ['/site/login']])
